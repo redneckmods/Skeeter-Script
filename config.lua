98bc2b44c6893ee9ee745bfaa22badce41388677
Scullyy <51968381+Scullyy@users.noreply.github.com>
2021-03-04 04:55:23 +0000

    Added label to config and fixed typo

--- config.lua
+++ config.lua
@@ -1,9 +1,10 @@
 config = {
+    label = "~b~Instructional Buttons: ~w~[~r~Beta Phase~w~]~n~~w~Press ~INPUT_CONTEXT~ to open~w~ compartment doors.~n~Press ~INPUT_DETONATE~ to toggle compartment lights.~n~Press ~INPUT_VEH_DUCK~ to toggle left scene lights.~n~Press ~INPUT_VEH_HEADLIGHT~ to toggle right scene lights.~n~Press ~INPUT_CELLPHONE_CAMERA_GRID~ to toggle rear scene lights.~n~Press ~INPUT_REPLAY_STARTPOINT~ to toggle rear ground lights.",
     skeeter_vehicle = {
         "16ramcskeeter",
         "16ramsskeeter",
         "20ramcskeeter",
-        "20ramcskeeter",
+        "20ramsskeeter",
         "f550cskeeter",
         "f550sskeeter"
     }
